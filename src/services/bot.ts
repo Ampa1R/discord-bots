09942853d352d62b4ecf347a888fcb14b983c9a1
JokerCerg <3112joker3112@gmail.com>
2021-02-04 01:46:43 +0300

    fix duplicate memes, add randomise message phrase from array, add sendig alternate memes if current best meme was posted

--- src/services/bot.ts
+++ src/services/bot.ts
@@ -17,9 +17,9 @@ export default class BotService {
         this.client.login(this.token);
 
         this.client.on('ready', async () => {
-            console.log('I\'m ready');
+            console.log("I'm ready!");
             this.sendMeme();
-            setInterval(() => this.sendMeme(), 86400000);
+            setInterval(() => this.sendMeme(), 10800000);
         });
 
         // this.client.on('message', async (message: Discord.Message) => {
@@ -29,11 +29,15 @@ export default class BotService {
         // });
     }
 
-    private async sendMeme() {
+    private async sendMeme(): Promise<void> {
         const memeUrl = await this.getUnpostedMeme();
 
-        const attachment = new Discord.MessageAttachment(memeUrl);
+        if (!memeUrl) {
+            console.log('All actual memes has been posted');
+            return;
+        }
 
+        const attachment = new Discord.MessageAttachment(memeUrl);
         const channel = this.client.channels.resolve(this.channelId);
         if (channel instanceof Discord.TextChannel) {
             channel.send(generateMemeComment(), attachment);
@@ -41,17 +45,23 @@ export default class BotService {
         }
     }
 
-    private async getUnpostedMeme(): Promise<string> {
-        const url = await this.memeService.getImageUrl();
-        const isMemePosted = await this.redisService.exists(`${this.postedMemesPrefix}${url}`);
-        
-        // TODO
-        if (isMemePosted) {
+    private async getUnpostedMeme(): Promise<string | undefined> {
+        const urls = await this.memeService.getMemeUrls();
+
+        for(let url of urls) {
+            if (await this.isUnpostedMeme(url)) return url;
+        }
+        return undefined;
+    }
+
+    private async isUnpostedMeme(url: string): Promise<boolean> {
+        const isPosted = await this.redisService.exists(`${this.postedMemesPrefix}${url}`);
+
+        if (isPosted) {
             console.log(`meme ${url} already has been posted`);
         } else {
             console.log(`meme ${url} is posted for the first time`);
         }
-        
-        return url;
+        return !isPosted;
     }
 }
